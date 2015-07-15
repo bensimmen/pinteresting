b5e17e238d4af0e346eb6c6276353a4251ac447e
Ben Simmen <ben@stzuri.co.nz>
2015-07-15 15:31:16 +0200

    small bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,6 @@ Rails.application.configure do
 
   # Required for Heroku
   # Note to set this to your actual host
-  config.action_mailer.default_url_options = { host: => 'ben-pinteresting.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'ben-pinteresting.herokuapp.com'}
 
 end
